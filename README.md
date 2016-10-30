1d59f84ab3df7636f71ef564d57194b42b7a0118
Bryan Alexander <calbryan@gmail.com>
2016-10-30 13:52:17 -0700

    initial commit with README and first two exercises

---
+++ README.md
@@ -0,0 +1,3 @@
+#Node.js Essential Training
+This repository contains exercises from Lynda.com Node.js Essential Training.
+
---
+++ global.js
@@ -0,0 +1,6 @@
+//exercise in working with the console object
+
+var path = require("path");
+
+console.log(`Rock on World from ${path.basename(__filename)}`);
+
