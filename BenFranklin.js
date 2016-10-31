48a96e7919c9ce2774a54935331295f084b718a8
Bryan Alexander <calbryan@gmail.com>
2016-10-31 15:09:26 -0700

    covered export.modules and require custom lib

--- BenFranklin.js
+++ BenFranklin.js
@@ -20,6 +20,8 @@ emitter.emit('customEvent', "Hello World", 200);
 
 //second example
 
+/* code below moved to Person.js to demonstrate modules
+
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
@@ -31,12 +33,24 @@ var Person = function(name) {
 
 util.inherits(Person, EventEmitter); // adds EventEmitter to Person
 
-var ben = new Person("Ben Franklin");
+*/
 
-ben.on('speak', function(said){
+var Person = require("./lib/Person"); //custom modules requires Path
+
+var ben = new Person("Ben Franklin");
+var george = new Person("George Washington");
+
+george.on('speak', function(said) {
+
+  console.log(`${this.name} -> ${said}`);
+
+});
+
+ben.on('speak', function(said) {
 
   console.log(`${this.name}: ${said}`);
 
 });
 
 ben.emit('speak', "You may delay, but time will not.");
+george.emit('speak', "It is far better to be alone, than to be in bad company.");
