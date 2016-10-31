8642927da5046084cb892b9ea7ba8de0e106c30d
Bryan Alexander <calbryan@gmail.com>
2016-10-30 18:28:00 -0700

    completed exercise introducing core modules

---
+++ core.js
@@ -0,0 +1,15 @@
+//exercise introducing the require, util, and v8 modules
+
+var path = require('path');
+var util = require('util');
+var v8 = require('v8');
+
+
+util.log(path.basename(__filename) );
+
+var dirUploads = path.join(__dirname, 'www', 'files', 'uploads');
+
+util.log(dirUploads);
+
+util.log(v8.getHeapStatistics());
+
