eb4b384a05db4d1d70d7638b84ae8dfc7d4ebad6
Bryan Alexander <calbryan@gmail.com>
2016-11-03 11:18:42 -0700

    completed exercise requesting a file over https

---
+++ request.js
@@ -0,0 +1,62 @@
+// exercise making a request with the https module
+
+var https = require("https");
+var fs = require("fs");
+
+var options = {     // object literal of options to complete the request -- details of request
+
+  hostname: "en.wikipedia.org",
+  port: 443,                        // https 443 | http 80
+  path: "/wiki/George_Washington",
+  method: "GET"
+
+};
+
+var req = https.request(options, function(res) {
+
+  var responseBody = "";
+
+  console.log("Response from server started.");
+  console.log(`Server Status: ${res.statusCode} `);
+  console.log("Response Headers: %j", res.headers);
+
+  res.setEncoding("UTF-8");
+
+  res.once("data", function(chunk) {
+
+    console.log(chunk);
+
+  });
+
+  res.on("data", function(chunk) {
+
+    console.log(`--chunk-- ${chunk.length}`);
+    responseBody += chunk;
+
+  });
+
+  res.on("end", function() {
+
+    fs.writeFile("george-washington.html", responseBody, function(err) {
+
+      if (err) {
+
+        throw err;
+
+      }
+
+      console.log("File Downloaded");
+
+    });
+
+  });
+
+});
+
+req.on("error", function(err) {
+
+  console.log(`problem with request: ${err.message}`);
+
+});
+
+req.end();
