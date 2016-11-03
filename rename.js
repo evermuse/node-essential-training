1885b3c775dd70c26dc8274f8c5371c257d88bc2
Bryan Alexander <calbryan@gmail.com>
2016-11-03 09:46:54 -0700

    completed exercises covering rename and remove methods of fs

---
+++ rename.js
@@ -0,0 +1,21 @@
+// exercise showing rename of files
+
+var fs = require("fs");
+
+fs.renameSync("./lib/project-config.js", "./lib/config.json");
+
+console.log("Config.json file renamed");
+
+fs.rename("./lib/notes.md", "./notes.md", function(err) {
+
+  if (err) {
+
+    console.log(err);
+
+  } else {
+
+    console.log("Success!");
+
+  }
+
+});
